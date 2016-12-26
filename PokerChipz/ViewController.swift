e47b0e8c28e824e73734b7fbb29edd23c3c52369
Eric Zhang <zhangeric99@gmail.com>
2016-12-26 03:33:37 -0800

    Initial Commit

---
+++ PokerChipz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokerChipz
+//
+//  Created by Eric Zhang on 12/26/16.
+//  Copyright © 2016 Eric Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
